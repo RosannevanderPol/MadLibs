eb7e127e8728985215dd6ac78596f82cd9bd01ae
RosannevanderPol <ccroos@gmail.com>
2015-11-13 20:36:55 +0100

    3e

--- app/src/main/java/com/example/rosanne/madlibs/Start.java
+++ app/src/main/java/com/example/rosanne/madlibs/Start.java
@@ -2,13 +2,8 @@ package com.example.rosanne.madlibs;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Start extends AppCompatActivity {
 
@@ -19,18 +14,10 @@ public class Start extends AppCompatActivity {
     }
 
     public void GetstartedClick(View view) {
-        Intent Getstarted = new Intent(getApplicationContext(), SecondScreen.class);
+        Intent Getstarted = new Intent(this, SecondScreen.class);
+        Getstarted.putExtra("callingActivity", "MainActivity");
         startActivity(Getstarted);
     }
-
-    public void GomakemystoryClick(View view) {
-        Intent Gomakemystory = new Intent(this, ThirdScreen.class);
-
-        final int result = 1;
-
-        Gomakemystory.putExtra("callingActivity", "MainActivity");
-
-        startActivity(Gomakemystory);
-    }
-
 }
+
+
